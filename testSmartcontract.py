156bbecde55a74465fec532af045aef7d210d0d0
Divyyesh Chandra <divyyesh.bvp23@gmail.com>
2023-03-30 19:31:50 +0530

    Added testing smart contract script

--- testSmartcontract.py
+++ testSmartcontract.py
@@ -17,7 +17,7 @@ privateKey=os.getenv("PrivateKey")
 
 
 def TransferToken():
-        if date.today().day !=15:
+        if date.today().day !=16:
                 return
         
         web3=Web3(Web3.HTTPProvider(alchemyURL))
@@ -29,32 +29,35 @@ def TransferToken():
         Wallet_from=Web3.to_checksum_address(PublicAddress)
         Wallet_to=Web3.to_checksum_address(SendingAddress)
 
-        # totalTokenSupply=contract.functions.totalSupply().call()
-        # totakTokens=web3.from_wei(totalTokenSupply,'ether')
+        token_balance = contract.functions.balanceOf(Wallet_to).call()
+        totalTokenSupply=contract.functions.totalSupply().call()
+        totakTokens=web3.from_wei(token_balance,'ether')
         # print(totalTokenSupply)
+        # print(token_balance)
         # print(totakTokens)
+        print(totakTokens)
 
 
-        nonce=web3.eth.get_transaction_count(Wallet_from)
+        # nonce=web3.eth.get_transaction_count(Wallet_from)
 
 
-        contract_txn=contract.functions.transfer(
-               Wallet_to,
-               10*10**18,
-        ).build_transaction(
-            {
-                'chainId': 80001,
-                'nonce': nonce,
-                'gas': 100000,
-                'gasPrice': web3.to_wei("10", "gwei")
-            }
-        )
+        # contract_txn=contract.functions.transfer(
+        #        Wallet_to,
+        #        10*10**18,
+        # ).build_transaction(
+        #     {
+        #         'chainId': 80001,
+        #         'nonce': nonce,
+        #         'gas': 100000,
+        #         'gasPrice': web3.to_wei("10", "gwei")
+        #     }
+        # )
 
-        signed_tx=web3.eth.account.sign_transaction(contract_txn,privateKey)
+        # signed_tx=web3.eth.account.sign_transaction(contract_txn,privateKey)
 
-        tx_hash=web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        # tx_hash=web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
-        print(web3.to_hex(tx_hash))
+        # print(web3.to_hex(tx_hash))
 
 
 if __name__ == "__main__":
